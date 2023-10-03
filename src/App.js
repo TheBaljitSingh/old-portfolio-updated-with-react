87c6d6419404f25ff8386ae0de65cf1df022e46c
Baljit Singh <baljitsingh692002@gmail.com>
2023-10-03 14:22:43 +0530

    updated version

--- src/App.js
+++ src/App.js
@@ -18,18 +18,6 @@ import Navbar from "./Navbar";
 function App() {
 
 
-  // const [stickyDiv, setStickyDiv] = useState("");
-
-  // useEffect(()=>{
-
-  // window.addEventListener("scroll", sti)
-
-  //   return()=>{
-
-  //   };
-
-  // }, []);
-
   const items = 
     Friends.map((e) => (
         
@@ -79,8 +67,8 @@ function App() {
 
 
   return (
-    <div >
-      <div className=" min-h-screen max-h-screen w-full bg-[url('./images/banner.jpg')] bg-cover bg-center ">
+    <div className="overflow-hidden" >
+      <div className=" min-h-screen max-h-screen w-screen bg-[url('./images/banner.jpg')] bg-cover bg-center ">
         {/* leking ye white wala nahi hat raha hai isko hatana hai */}
 
       <Navbar/>
@@ -106,17 +94,17 @@ function App() {
 
 
       {/* about me section start */}
-      <div className="w-full h-[88vh]  bg-white">
+      <div className="w-screen md:h-[88vh] h-auto  bg-white ">
 
-        <div className="flex flex-col items-center justify-center mt-8">
+        <div className=" flex flex-col items-center justify-center mt-8">
           <h1 className="text-3xl font-bold">About me</h1>
           <div className="w-32 h-1 border-b-4 border-blue-500 mt-2 rounded-xl "></div>
           <p className="font-bold ">who i am</p>
         </div>
 
-        <div className="flex justify-center items-center mt-16">
+        <div className=" md:justify-center md:items-center mt-16 flex flex-col md:flex-row">
 
-          <div className="ml-24">
+          <div className="md:ml-24  flex items-center justify-center">
             <img className="max-w-sm border rounded-2xl object-center object-cover" src="https://thebaljitsingh.github.io/portfolio/gallery/Baljit_maskup.jpg" alt="maskup" />
           </div>
           <div className="p-14 flex-wrap">
@@ -130,29 +118,29 @@ function App() {
       </div>
 
       {/* Experiance section start here */}
-      <div className="w-full h-[70vh] bg-blue-500">
+      <div className="w-full md:h-[70vh] md:h:auto h-[96vh] bg-blue-500">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-3xl font-bold mt-8">My Experiance</h1>
           <div className="w-40 h-1 border-b-4 border-white mt-4 rounded-xl"></div>
           <p className="font-semibold -mt-4 bg-blue-500  w-[100px] flex justify-center"> what i have</p>
         </div>
 
-        <div className="flex items-center  justify-center mt-20 w-34 ">
-          <div className="bg-black text-white w-1/4 p-2 m-2 h-48 text-center rounded border-black">
+        <div className="flex md:items-center md:justify-center md:flex-row flex-col items-center md:mt-20 mt:0 w-34 ">
+          <div className="bg-black text-white md:w-1/4 w-[460px] p-2 m-2 h-48 text-center rounded border-black">
             <div className="hover:scale-105 cursor-pointer ease-in-out duration-500 hover:text-gray-400">
               <div className="flex justify-center mt-2">
               < BsWordpress size={50}/>
               </div>
               <div className="mt-4">
               <h2 className="font-medium text-2xl my-2">Wordpress</h2>
-              <p className="mt-2">From installing to building a dynamic website</p>
+              <p className="mt-2 text-center">From installing to building a dynamic website</p>
 
               </div>
               
             </div>
 
           </div>
-          <div className="bg-black text-white w-1/4 p-2 m-2 h-48 text-center rounded border-black " >
+          <div className="bg-black text-white md:w-1/4 w-[460px]  p-2 m-2 h-48 text-center rounded border-black " >
             <div className="hover:scale-105 cursor-pointer ease-in-out duration-500 hover:text-gray-400">
               {/* <img className="w-[65px] mx-auto -mt-1  " src="" alt="" /> */}
               <div className="flex justify-center mt-2">
@@ -160,7 +148,7 @@ function App() {
               </div>
               <div className="mt-4">
               <h2 className="font-medium text-2xl my-auto ">SEO</h2>
-              <p className="mt-2 ">Learning and doing SEO in last 2 year website listing on Google</p>
+              <p className="mt-2 text-center">Learning and doing SEO in last 2 year website listing on Google</p>
 
 
               </div>
@@ -168,7 +156,7 @@ function App() {
             </div>
 
           </div>
-          <div className="bg-black text-white w-1/4 p-2 m-2 h-48 text-center rounded border-black">
+          <div className="bg-black text-white md:w-1/4 w-[460px] p-2 m-2 h-48 text-center rounded border-black">
             <div className="hover:scale-105 cursor-pointer ease-in-out duration-500 hover:text-gray-400">
               <div className="mt-2">
               <img className="w-[105px] justify-center mx-auto -m-4" src="https://www.inventicons.com/uploads/iconset/2438/wm/512/Responsive-Web-Design-95.png" alt="" />
@@ -188,28 +176,29 @@ function App() {
       </div>
 
       {/* My Skills Status */}
-      <div className="w-full h-screen bg-white">
-        <div className="flex flex-col items-center justify-center">
+      <div className="md:w-full md:h-screen h-auto  max-w-full w-screen bg-white">
+        <div className="flex flex-col items-center justify-center w-screen">
           <h1 className="text-3xl font-bold mt-8">My Skills</h1>
           <div className="w-40 h-1 border-b-4 border-blue-500 mt-4 rounded-xl"></div>
           <p className="font-semibold -mt-4 bg-white  w-[100px] flex justify-center"> what i know</p>
         </div>
 
         <div className="  ">
-          <div className=" p-4 w-[70%]ml-20  flex items-center mt-24">
+          <div className=" p-4 w-auto md:ml-20  md:flex md:items-center flex md:flex-row flex-col max-h-auto md:mt-24 mt-10 ">
 
-          <div className=" w-1/2 ml-20">
+          <div className=" md:w-1/2 w-auto md:ml-20 ml:10">
             <h2 className="font-medium text-2xl">My creative skills & experience</h2>
-            <p className=" text-lg mt-4 text-justify">I am interested in Web Devlopment, Competative Programming, and learning new Programming Language and seeking excitng opporunies replaced to the same. In the Web Devlopment field, I am looking for being a Full Stack Web Devloper. I am also looking forward to Android Devlopment in the upcomming academic year. I'm started Open-Source Contribution. I am also trying to do freelancing on different platforms as a side hustle.
+            <p className=" text-lg mt-4 md text-justify break-words">I am interested in Web Devlopment, Competative Programming, and learning new Programming Language and seeking excitng opporunies replaced to the same. In the Web Devlopment field, I am looking for being a Full Stack Web Devloper. I am also looking forward to Android Devlopment in the upcomming academic year. I'm started Open-Source Contribution. I am also trying to do freelancing on different platforms as a side hustle.
               Begning in High Schools I learned to create a website on WordPress by watching videos from YouTube(SidTalk). Simultaneously I also learned SEO.
               That skills that i have attained from the dynamic environment and college life, and I am looking forward to implementing and pushing them in any opportunity that i receive.</p>
               <button className="p-4 my-2 font-medium bg-indigo-700 hover:text-indigo-700 hover:bg-transparent border-2 rounded-lg border-indigo-700 w-[150px] text-white "><a href="/#">Read more</a></button>
 
 
           </div>
-          <div className=" ml-20 mr-10 p-4 h-auto w-1/2">
-            
 
+          {/* progress bars */}
+          <div className=" md:ml-20 mr-20 p-4 h-auto md:w-1/2 w-full">
+            
           <div class="flex justify-between mb-1">
             <span class="text-base font-medium text-black dark:text-black ">HTML</span>
             <span class="text-sm font-medium text-black dark:text-black">90%</span>
@@ -257,7 +246,7 @@ function App() {
       </div>
 
             {/* my friend section */}
-      <div className="w-full h-[80vh] bg-blue-400">
+      <div className="w-full h-[80vh] hidden bg-blue-400">
 
       <div className="flex flex-col items-center justify-center">
           <h1 className="text-4xl font-bold mt-8">My College Friends</h1>
@@ -265,7 +254,7 @@ function App() {
           <p className="font-semibold text-xl -mt-5 bg-blue-400  w-[140px] flex justify-center"> who with me</p>
         </div>
 
-        <div className=" ml-12 mr-12">     
+        <div className=" mx-10">     
           <div className=" flex-row mt-24 scroll-smooth "> 
 
           <div className="app ">
@@ -280,19 +269,19 @@ function App() {
 
 
       {/* contact me section */}
-      <div className=" bg-white w-full h-[80vh] ">
+      <div className=" bg-blue-400  w-full h-[80vh] ">
         <div className="flex flex-col items-center justify-center">
             <h1 className="text-4xl font-bold mt-8 ">Contact me</h1>
             <div className="w-40 h-1 border-b-4 border-blue-700 mt-5 rounded-xl"></div>
-            <p className="font-semibold text-xl -mt-5 bg-white   w-[120px] flex justify-center"> get in touch</p>
+            <p className="font-semibold text-xl -mt-5 bg-blue-400   w-[120px] flex justify-center"> get in touch</p>
         </div>
 
         <div className="flex  flex-col">
-          <div className="ml-44 mt-12">
+          <div className="md:ml-44 ml-24 mt-12">
             <h2 className="font-bold text-2xl ">Get in Touch</h2>
             <p className="text-xl font-normal mt-2">for any query or support so please reach out me drop me messege</p>
           </div>
-            <div className=" mt-6 ml-44 space-y-4 ">
+            <div className=" mt-6 md:ml-44 ml-24 space-y-4 ">
               <div className="flex flex-row space-x-8 ">
                 <div className="flex items-center">
                 <FaUser size={30}/>
@@ -327,7 +316,7 @@ function App() {
             </div>
 
             
-            <div className="ml-44 mt-4">
+            <div className="md:ml-44 ml-24   mt-4">
               <button className="my-2 font-medium bg-indigo-700  hover:text-indigo-700 hover:bg-transparent border-2 p-4 rounded-lg border-indigo-700 w-[150px] text-white "><a href="https://wa.me/8709548015">Whatsapp me!</a></button>
             </div>
             
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,41 +4,80 @@ import { useState } from "react"
 export default function Navbar(){
  
     const [fix, setFix] = useState(false);
+    const [menu, setMenu] = useState(false);
 
     function setFixed(){
-         if(window.scrollY>=200){
+         if(window.scrollY>=150){
             setFix(true);   
          }else{
             setFix(false);
          }
     }
     window.addEventListener("scroll", setFixed)
-    console.log(fix);
+
+  
+
+    const handleMenu = ()=>{
+        setMenu(!menu);
+    }
 
     return(
         <div>
+            <div>
+                { menu ? 
+                <div className="w-full bg-gray-500 h-screen fixed z-50 ">
+                    <div className="">
+                    <div className=" absolute top-6 text-4xl text-white font-bold mx-3 ">
+                    thebaljit<span className="text-indigo-700">singh</span>
+                    
+                    </div>
+                    <div className=" absolute top-6 right-4 block">
+                                <button onClick={handleMenu} className=" text-4xl text-black flex justify-between sm:hidden "><a href="/#">&#8801;</a></button>
+                    </div>
+                    
+                    </div>
+                    <div className="flex flex-col items-center justify-center p-9 ">
+                        
+                        <div className=" mt-8 font-medium text-2xl text-center text-black">
+                            
+                            
+                        <ul className="menu-btn"><a href="/#">Home</a></ul>
+                        <ul className="menu-btn"><a href="/#">About</a></ul>
+                        <ul className="menu-btn"><a href="/#">Services</a></ul>
+                        <ul className="menu-btn"><a href="/#">Skills</a></ul>
+                        <ul className="menu-btn"><a href="/#">Team</a></ul>
+                        <ul className="menu-btn"><a href="/#">Contact me</a></ul>
+                        </div>
+                    </div>
+
+            </div>:
+            <nav className={!fix?"w-full md:max-h-24 flex justify-between mx-auto items-center px-4 h-20 :px-4 fixed top-0 bg-transparent z-50 ":"w-full max-h-24 flex justify-between mx-auto items-center px-4 md:px-4 fixed  top-0 bg-black h-20 ease-in-out duration-150 z-50"}>
+
+
+            <div className="mt-2 text-4xl text-white font-bold md:mx-10 ">
+              thebaljit<span className="text-indigo-700">singh</span>
+  
+            </div>
+            {/* main menu ye wala hai */}
+            {/* menu ka margin me dikkat aa rahi hai. isko sabse right me karna hai */}
+            <div className=" list-none md:flex  md:justify-between md:p-4 md:ml-[300px]  text-white font-semibold text-xl hidden ">
+              <ul className="mx-[10px]"><a href="/#">About</a></ul>
+              <ul className="mx-[10px]"><a href="/#">Home</a></ul>
+              <ul className="mx-[10px]"><a href="/#">Services</a></ul>
+              <ul className="mx-[10px]"><a href="/#">Skills</a></ul>
+              <ul className="mx-[10px]"><a href="/#">Team</a></ul>
+              <ul className="mx-[10px]"><a href="/#">Contact</a></ul>
+            </div>
+            <div>
+              <button  id="menu" onClick={handleMenu} className=" text-4xl mt-2 text-white flex md:hidden "><a href="/#">&#8801;</a></button>
+  
+            </div>
+          </nav>
+
+                 }
+            </div>
             
-            <nav className={!fix?"w-full max-h-24 flex justify-between mx-auto items-center px-4 md:px-4 fixed top-0 bg-transparent z-50":"w-full max-h-24 flex justify-between mx-auto items-center px-4 md:px-4 fixed  top-0 bg-black ease-in-out duration-500 z-50"}>
-
-
-          <div className="text-4xl text-white font-bold md:mx-10 ">
-            thebaljit<span className="text-indigo-700">singh</span>
-
-          </div>
-          {/* menu ka margin me dikkat aa rahi hai. isko sabse right me karna hai */}
-          <div className=" list-none md:flex md:p-4 md:ml-[300px]  text-white font-semibold text-xl hidden ">
-            <ul className="mx-[10px]"><a href="/#">About</a></ul>
-            <ul className="mx-[10px]"><a href="/#">Home</a></ul>
-            <ul className="mx-[10px]"><a href="/#">Services</a></ul>
-            <ul className="mx-[10px]"><a href="/#">Skills</a></ul>
-            <ul className="mx-[10px]"><a href="/#">Team</a></ul>
-            <ul className="mx-[10px]"><a href="/#">Contact</a></ul>
-          </div>
-          <div>
-            <button className="md:hidden text-4xl text-white flex "><a href="/#">&#8801;</a></button>
-
-          </div>
-        </nav>
+            
 
         </div>
     )
